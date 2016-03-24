96248ef7c2a2009c4962a37a75e799fefa57200b
Alec Tunbridge <alec.tunbridge@gmail.com>
2016-03-24 15:05:17 +0000

    "[GIT AUTO COMMIT]: Thu Mar 24 2016 16:05:16 GMT+0100 (GMT Daylight Time)"

--- src/main/java/StoreCredit.java
+++ src/main/java/StoreCredit.java
@@ -2,7 +2,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StoreCredit {
     public static void main(String[] args) throws IOException {
@@ -12,9 +15,18 @@ public class StoreCredit {
 
         strings.remove(0);
 
-        System.out.println(strings.get(0));
+        for (int i = 0; i < strings.size(); i += 3) {
+                String credit = strings.get(i);
+                List<Integer> items = Stream.of(strings.get(i + 2)
+                        .split(" "))
+                        .map(Integer::valueOf).peek(System.out::println).collect(Collectors.toList());
 
+
+        }
+
+//        formateedList = thingsToBuy();
     }
+
 
 
 }
