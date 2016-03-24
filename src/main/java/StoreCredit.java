5aba79be5242f704f08dc923c1228f4639d75536
Alec Tunbridge <alec.tunbridge@gmail.com>
2016-03-24 14:43:17 +0000

    "[GIT AUTO COMMIT]: Thu Mar 24 2016 15:43:16 GMT+0100 (GMT Daylight Time)"

--- src/main/java/StoreCredit.java
+++ src/main/java/StoreCredit.java
@@ -1,2 +1,13 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class StoreCredit {
+    public static void main(String[] args) {
+        Path path = Paths.get("A-small-practice.in");
+
+        List<String> strings = Files.readAllLines(path);
+
+    }
 }
